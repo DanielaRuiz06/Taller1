591fe4ef28faa152fa0bc4f43d65627936d9f473
Paula Rosas <paula.rosas02@uptc.edu.co>
2026-02-16 23:13:27 -0500

    Administrador implementacion de metodos

--- src/co/edu/uptc/interfaces/PresenterInterface.java
+++ src/co/edu/uptc/interfaces/PresenterInterface.java
@@ -6,5 +6,5 @@ import co.edu.uptc.model.UnidadMedida;
 public interface PresenterInterface {
     public void setView (ViewInterface view);
     public void setModel (ModelInterface model);
-    public Producto createProduct (String description, double price, UnidadMedida medida );
+    public Producto createProduct (String description, double price, UnidadMedida medida ) throws Exception;
 }
--- src/co/edu/uptc/util/Utilities.java
+++ src/co/edu/uptc/util/Utilities.java
@@ -4,5 +4,6 @@ public class Utilities {
     public final static String [] MENU_PRINCIPAL = {"1. Adicionar un Producto", "2. Mostrar los productos","3. Mostar los productos ordenados","4. Borrar un producto de la lista"};
     public final static String MENSAJE_ELIMINAR = "Por favor, ingrese el nombre del producto que desea eliminar";
     public final static String MENSAJE_BIENVENIDA = "Bienvenido usuario!! \nPor favor,ingrese el número de la opción que desea realizar:";
+    public final static String MENSAJE_ENUM = "Digite el valor numérico de la opción que desea: "+"\n"+"0. Cancelar";
 
 }
