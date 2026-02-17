6093d23c1c1907a85c3da2192ba220d8b79b33dc
DanielaRuiz06 <paula.ruiz06@uptc.edu.co>
2026-02-16 21:56:59 -0500

    cuarto cambio Daniela Ruiz

---
+++ src/co/edu/uptc/util/Utilities.java
@@ -0,0 +1,8 @@
+package co.edu.uptc.util;
+
+public class Utilities {
+    public final static String [] MENU_PRINCIPAL = {"1. Adicionar un Producto", "2. Mostrar los productos","3. Mostar los productos ordenados","4. Borrar un producto de la lista"};
+    public final static String MENSAJE_ELIMINAR = "Por favor, ingrese el nombre del producto que desea eliminar";
+    public final static String MENSAJE_BIENVENIDA = "Bienvenido usuario!! \nPor favor,ingrese el número de la opción que desea realizar:";
+
+}
